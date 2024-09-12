ff6ca1a1acfcf6f7eeb983915792d378f1f43d47
Armando Valdez <155396846+valde148@users.noreply.github.com>
2024-09-12 13:19:47 -0500

    changes

--- binpacking.java
+++ binpacking.java
@@ -3,8 +3,18 @@ import java.util.Scanner;
 import java.util.Collections;
 
 public class binpacking{
+
+    public static void packing(int[] items, int n, int cap){
+        int[] bin1 = new int[n];
+        int [] bin2= new int[n];
+        int[] bin3 = new int[n];
+    }
     public static void main(String[]args){
         Scanner s = new Scanner(System.in);
         System.out.println("Bin Enter: ");
+
+        int amountBin = s.nextInt();
+        
+        System.out.println("Enter ")
     }
-}
+} 
