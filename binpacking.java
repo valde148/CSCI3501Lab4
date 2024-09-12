017152bf249cefcad85c81420543a4a9334b1d1f
Jaden Rainey <raine123@morris.umn.edu>
2024-09-12 14:15:19 -0500

    Base logic down, working on largestFit

--- binpacking.java
+++ binpacking.java
@@ -1,13 +1,43 @@
 //Armando Valdez & Jaden Rainey
 import java.util.Scanner;
+import java.util.ArrayList;
 import java.util.Collections;
+import java.util.List;
 
 public class binpacking{
 
-    public static void packing(int[] items, int n, int cap){
-        int[] bin1 = new int[n];
-        int [] bin2= new int[n];
-        int[] bin3 = new int[n];
+    public static void packing(List<Integer> items, int n, int cap){
+        List<Integer> bin1 = new ArrayList<Integer>(n);
+        List<Integer> bin2 = new ArrayList<Integer>(n);
+        List<Integer> bin3 = new ArrayList<Integer>(n);
+
+        int bin1Cap = 0;
+        int bin2Cap = 0;
+        int bin3Cap = 0;
+
+        Collections.sort(items);
+
+        for (int i = 0; bin1Cap < cap;){
+            if (bin1Cap <= 0){
+                bin1.set(i, items.get(i));
+                items.remove(i);
+            } else if (bin1Cap < cap){
+                int largest = largestFit(items, bin1Cap, cap);
+                bin1.set(i, largest);
+                items.remove(largest);
+            }
+        }
+    }
+
+    public int largestFit(List<Integer> items, int curcap, int cap){
+        int largest = 0;
+        for (int i = items.size() - 1; i > 0; i--){
+            if (items.get(i) + curcap <= cap){
+                largest = items.get(i);
+                break;
+            }
+        }
+        return largest;
     }
     public static void main(String[]args){
         Scanner s = new Scanner(System.in);
