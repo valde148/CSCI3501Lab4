627b784bc8a815b69ad3447666ddbd676f5cc8e9
Armando Valdez <155396846+valde148@users.noreply.github.com>
2024-09-12 13:04:07 -0500

    changes

--- binpacking.java
+++ binpacking.java
@@ -3,5 +3,8 @@ import java.util.Scanner;
 import java.util.Collections;
 
 public class binpacking{
-
+    public static void main(String[]args){
+        Scanner s = new Scanner(System.in);
+        System.out.println("Bin Enter: ");
+    }
 }
