bb5a06bd1b632de669631616b29940c2f9aa2b4f
Armando Valdez <valde148@d.umn.edu>
2024-09-14 21:02:59 -0500

    new done

--- binpacking.java
+++ binpacking.java
@@ -1,88 +1,116 @@
 //Armando Valdez & Jaden Rainey
 import java.util.Scanner;
-import java.util.Collections;
 
-public class binpacking{
+public class BinPacking {
 
-    public static void packing(int[] items, int n, int cap){
+    // this method packs items into bins by using first fit decreasing
+    // it also handles left overs
+    public static void packing(int[] items, int n, int cap) {
+        //we are using array as bins. 
         int[] bin1 = new int[n];
-        int [] bin2= new int[n];
+        int[] bin2 = new int[n];
         int[] bin3 = new int[n];
+        int[] leftovers = new int[n]; //stores items leftover 
 
-        int binsum = 0;
+        //it sums each bin to track the total amount of space in bins
+        int binsum1 = 0;
         int binsum2 = 0;
         int binsum3 = 0;
-        int binindex =;
-        int binindex2 =0;
-        int binindex3 = 0;
+        int bin1Index = 0;
+        int bin2Index = 0;
+        int bin3Index = 0;
+        int leftoverIndex = 0; // its for bins and leftovers
 
-        for(int i = 0; i<n -1; i++){
-            for(int j = 0; j < n -i -1; j++){
-                if (items[j] < items[j+1]){
-                    int t = items[j];
-                    items[j] = items[j+1];
-                    items[j+ 1] = t;
+        // sort the items in descending order using bubble sort 
+        // which is first fit decreasing
+        for (int i = 0; i < n - 1; i++) {
+            for (int j = 0; j < n - i - 1; j++) {
+                if (items[j] < items[j + 1]) {
+                    int temp = items[j];
+                    items[j] = items[j + 1];
+                    items[j + 1] = temp;
                 }
             }
         }
-        for(int i = 0; i< n; i++){
-            int x = items[i];
 
-            if(binsum + x <= cap){
-                bin1[binindex++] = x;
-                binsum1 = binsum2 + x;
-            }
+        //placing each item in the first available bin 
+        for (int i = 0; i < n; i++) {
+            int x = items[i]; // x will be the current item
 
-            else if(binsum2 + x <= cap){
-                bin2[binindex2 ++] = x;
-                binsum2 = binsum2 +x;
-            }
-            else if(binsum3 + x <= cap){
-                bin3[binindex3++] = x;
+            if (binsum1 + x <= cap) { //placing in all three bins
+                bin1[bin1Index++] = x;
+                binsum1 += x;
+            } else if (binsum2 + x <= cap) { 
+                bin2[bin2Index++] = x;
+                binsum2 += x;
+            } else if (binsum3 + x <= cap) {
+                bin3[bin3Index++] = x;
                 binsum3 += x;
+            } else { // what ever is left over 
+                leftovers[leftoverIndex++] = x;
             }
         }
 
+        // Output the contents 
+        System.out.print("Bin1: [");
+        for (int i = 0; i < bin1Index; i++) {
+            System.out.print(bin1[i]);
+            if (i < bin1Index - 1) 
+            System.out.print(", ");
+        }
+        System.out.println("]");
 
-    System.out.println("Bin 1: ");
-    for(int i = 0; i < binindex; i++){
-        System.out.print(bin1[i] +" ");
+        System.out.print("Bin2: [");
+        for (int i = 0; i < bin2Index; i++) {
+            System.out.print(bin2[i]);
+            if (i < bin2Index - 1) 
+            System.out.print(", ");
+        }
+        System.out.println("]");
+
+        System.out.print("Bin3: [");
+        for (int i = 0; i < bin3Index; i++) {
+            System.out.print(bin3[i]);
+            if (i < bin3Index - 1) 
+            System.out.print(", ");
+        }
+        System.out.println("]");
+
+        // Output any leftover items 
+        if (leftoverIndex > 0) {
+            System.out.print("Leftover items: [");
+            for (int i = 0; i < leftoverIndex; i++) {
+                System.out.print(leftovers[i]);
+                if (i < leftoverIndex - 1) 
+                System.out.print(", ");
+            }
+            System.out.println("]");
+        } else {
+            System.out.println("No leftover items.");
+        }
     }
-    System.out.println("unused space: " + (cap- binsum));
 
-    System.out.println("Bin 2: ");
-    for(int i = 0; i < binindex2; i++){
-        System.out.print(bin2[i] + " ");
-    }
-    System.out.println("unused space: " + (cap - binsum2));
-
-    System.out.println("Bin 3: ");
-    for(int i = 0; i < binindex3; i++){
-        System.out.print(bin3[i] + " ");
-    }
-    System.out.println("unused space: " + (cap - binsum3));
-
-    }
-
-
-    public static void main(String[]args){
+    public static void main(String[] args) {
         Scanner s = new Scanner(System.in);
 
-        System.out.println("Bin Cap Enter: ");
+        // the input for bin capacity
+        System.out.println("Enter B: ");
         int b = s.nextInt();
 
-        System.out.println("Number items: ");
+        // this will be the input for number of items
+        System.out.println("Enter N: ");
         int n = s.nextInt();
-        
-        int[] y = new int[n];
 
-        System.out.println("items: ");
-        for(int i = 0; i < n; i++){
-            y[i] = s.nextInt();
+        // the acutal input of the items
+        int[] items = new int[n];
+        System.out.println("Enter the items: ");
+        for (int i = 0; i < n; i++) {
+            items[i] = s.nextInt();
         }
-        packing(y, n, bc)
+
+        //calling the packing function in order to distribute items into bins
+        packing(items, n, b);
     }
+}
 
-
-
-} 
+//when writing this program it came with alot of 
